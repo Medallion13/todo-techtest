458974378a01a49db14c400d8414ee9cae2f0a5d
Medallion <nickcar13@gmail.com>
2025-11-20 09:35:59 -0500

    feat(stack): adding dynamo table with single table structure

--- infrastructure/stacks/todo_stack.py
+++ infrastructure/stacks/todo_stack.py
@@ -1,8 +1,29 @@
-from aws_cdk import Stack
+from aws_cdk import (
+    RemovalPolicy,
+    Stack,
+    aws_dynamodb as dynamodb,
+)
 from constructs import Construct
 
 
 class TodoStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope=scope, id=id, **kwargs)
-        pass
+
+        # Dynamo DB table definition
+
+        table = dynamodb.Table(
+            self,
+            "TodoTable",
+            table_name="TodoTable",
+            # partition key
+            partition_key=dynamodb.Attribute(name="PK", type=dynamodb.AttributeType.STRING),
+            # Sort key para queries
+            sort_key=dynamodb.Attribute(name="SK", type=dynamodb.AttributeType.STRING),
+            # Billing mode: pagar solo por uso ideal para el mvp, para un produccion revisar en funcion del uso
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            # Destroy para MVP revisar para produccion
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        self.table = table
